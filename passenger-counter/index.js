6e1ddd592d5129fbed24076331e2b845ce169418
abilling <30422483+abilling@users.noreply.github.com>
2022-04-09 00:24:29 -0500

    Uploaded completed files from local file system

--- passenger-counter/index.js
+++ passenger-counter/index.js
@@ -1,23 +1,36 @@
 // Starter Javascript File 
-//document.getElementById("count-el").innerText = 5;
+// Taking the id of h2 count-el, telling the js file to grab that
 
 let countEl= document.getElementById("count-el"); //pass-in argument, passing data for the function to use
+
+// telling the JS file to grab the p tag id, save-el to use in a function
+let saveEl = document.getElementById("save-el")
 
 console.log(countEl);
 
 let count = 0;
 
+// Increment function, it counts the button presses used by HTML ability onClick, and shows it on screen
+
 function increment() {
-	
+	//+= is variable + something, -= is variable - something
 	count +=1;
-	
-	countEl.innerText = count;
-	
+	//textContent works better than innerText, can read everything and is less expensive
+	countEl.textContent = count;
+	//Show in the console
 	console.log(count);
 	
 }
-
+// save function, it stores what we increment using a button press ability onClick, calling our function then resets
 function save() {
-	
-	console.log(count)
+	//countStr will show count -, putting the string first
+	let countStr = count + " - ";
+	//Rendering variable that has count and - in innerText
+	//increment countStr using textContext and our P tag save ID, it will show on HTML
+	saveEl.textContent += countStr;
+	console.log(count);
+	// Reset what it shows on screen, textContent is 0
+	countEl.textContent = 0;
+	//reset the actual count, count is now 0 and no longer increments unless we press the increment button
+	count = 0;
 }
