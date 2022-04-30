5cd54c49a67c5c825bbb42b0141a507922684c5a
abilling <30422483+abilling@users.noreply.github.com>
2022-04-30 03:05:31 -0500

    Added hasBlackjack and isAlive booleans

--- blackjack-app/index.js
+++ blackjack-app/index.js
@@ -2,6 +2,9 @@ let firstCard = 3;
 let secondCard = 10;
 
 let sum = firstCard + secondCard;
+let hasBlackjack = false;
+
+let isAlive = true;
 
 if (sum <= 20) {
 	console.log("Do you want to draw a new card? 🙂");
@@ -9,8 +12,14 @@ if (sum <= 20) {
 
 else if (sum === 21) {
 	console.log("You've got Blackjack! 🥳");
+	hasBlackjack = true;
 }
 
 else {
 	console.log("You're out of the game! 😭");
+	isAlive = false;
 }
+
+//cashout time
+console.log(hasBlackjack);
+console.log(isAlive);
