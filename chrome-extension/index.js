e2ccbe4e6582c2a7d07eb7d382408e14bd90ec02
Allison Billing <abilling@hawk.iit.edu>
2022-08-31 14:03:18 -0500

    Added new variables to js file

--- chrome-extension/index.js
+++ chrome-extension/index.js
@@ -1,5 +1,8 @@
-let inputBtn = document.getElementById("input-btn");
-
+//variable declaration
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+//added event listener
 inputBtn.addEventListener("click", function() {
     console.log("Button clicked!");
 })
