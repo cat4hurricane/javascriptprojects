12ee11eca92191e3329d6b2b4f124a43691e36a2
abilling <30422483+abilling@users.noreply.github.com>
2023-10-12 14:46:58 -0400

    created unordered list element for future rendering

--- chrome-extension/index.js
+++ chrome-extension/index.js
@@ -1,10 +1,17 @@
 // variable declaration \n
-const myLeads = []; // \n
+const myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]; // \n
 const inputEl = document.getElementById('input-el'); // \n
 const inputBtn = document.getElementById('input-btn'); // \n
+// grabbing unordered list and storing in comst variable
+const ulEl = document.getElementById("ul-el")
+
+console.log(ulEl)
 // added event listener // \n
 inputBtn.addEventListener('click', function() { // \n
   myLeads.push(inputEl.value)// \n
   console.log(myLeads)
 });
-// push to array
+// log out the items in the myLeads array using a for loop
+for (let i = 0; i < myLeads.length; i++) {
+  console.log(myLeads[i])
+}
