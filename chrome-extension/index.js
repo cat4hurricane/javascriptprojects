6920ce020c8bf5de067bed5087ddd7d1202f389e
abilling <30422483+abilling@users.noreply.github.com>
2023-10-12 14:11:25 -0400

    added push function/value to extension

--- chrome-extension/index.js
+++ chrome-extension/index.js
@@ -3,6 +3,8 @@ const myLeads = []; // \n
 const inputEl = document.getElementById('input-el'); // \n
 const inputBtn = document.getElementById('input-btn'); // \n
 // added event listener // \n
-inputBtn.addEventListener('click', () => { // \n
-  console.log('Button clicked!'); // \n
+inputBtn.addEventListener('click', function() { // \n
+  myLeads.push(inputEl.value)// \n
+  console.log(myLeads)
 });
+// push to array
